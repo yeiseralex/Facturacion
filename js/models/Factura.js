2bc71d5d56de38f451f3b572807c9c95197e571f
yeiseralex <alexser21@hotmail.com>
2019-01-25 19:49:45 -0500

    Creacion de Modelos
    - Se ha creado la carpeta js/models
    - Se crea el modelo de factura ( clase en javascript)

---
+++ js/models/Factura.js
@@ -0,0 +1,12 @@
+class Factura{
+
+    constructor(){
+
+        this.fact_id = 0;
+        this.fac_fecha = new Date();
+        this.fac_serie = "F001";
+        this.fac_numero = "0000023";
+    }
+      
+
+}
